47b33ef60032775e112d86eee2657b85ce9615b0
Darragh Mac Lochlainn <darraghmaclochlainn@gmail.com>
2019-10-29 00:44:58 +0000

    lab 3
    up to lab 3 done

---
+++ placemark-00/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,61 @@
+package org.wit.placemark.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_placemark.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.models.PlacemarkModel
+import org.wit.placemark.main.MainApp
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+    var placemark = PlacemarkModel()
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark)
+        app = application as MainApp
+
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+        info("Placemark Activity started..")
+
+        if (intent.hasExtra("placemark_edit")) {
+            placemark = intent.extras?.getParcelable<PlacemarkModel>("placemark_edit")!!
+            placemarkTitle.setText(placemark.title)
+            description.setText(placemark.description)
+        }
+
+        btnAdd.setOnClickListener() {
+            placemark.title = placemarkTitle.text.toString()
+            placemark.description = description.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                app.placemarks.create(placemark.copy())
+                info("add Button Pressed: ${placemark}")
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
+            } else {
+                toast("Please Enter a title")
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
